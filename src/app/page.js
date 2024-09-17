0da7660ceaa7ff5e7cf59c9d1eb678c9085631a7
Cassiana <cassianasilveira64@gmail.com>
2024-09-17 10:28:34 -0300

    Add historical part

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import { FaGithub } from "react-icons/fa6";
 import Main from "@/components/main";
+import References from "@/components/references";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="flex items-center center flex-col w-[100%]">
-        <h1 className="text-3xl font-extrabold">Curvas de Bezier -- Splines</h1>
+        <h1 className="text-3xl font-extrabold">Curvas de Bezier - Splines</h1>
         <h2 className="text-2xl self-end font-light">Cassiana Silveira</h2>
         <h2 className="text self-end font-thin">Métodos Matemáticos I</h2>
       </header>
@@ -14,12 +15,7 @@ export default function Home() {
       <Main />
 
       <footer className="flex flex-col gap-5 row-start-3 items-center w-[100%]">
-        <div className="flex flex-col gap-3 self-start">
-          <p className="text-md text-xl font-bold">
-            Referências: 
-          </p>
-          {/* TODO: Referências  */}
-        </div>
+        <References />
         <div className="flex gap-3 items-center">
           <p className="text-md">Disponível em</p>
           <a href="https://github.com/ifcassianasl/bezier-curves--splines" className="text-white text-4xl" target="_blank">
---
+++ src/components/references.js
@@ -0,0 +1,56 @@
+const References = () => {
+  return (
+    <div className="flex flex-col gap-3 self-start">
+      <p className="text-md text-xl font-bold">
+        Referências: 
+      </p>
+      {/* TODO: Referências  */}
+      <ul class="list-disc list-inside p-4">
+        <li class="mb-2">
+          WIKIPEDIA. Bézier curve. Disponível em:{' '}
+          <a href="https://en.wikipedia.org/wiki/B%C3%A9zier_curve" class="text-blue-500 underline">
+            https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+          </a>. 
+          Acesso em: 10 out. 2023.
+        </li>
+        <li class="mb-2">
+          WIKIPEDIA. Spline (mathematics). Disponível em:{' '}
+          <a href="https://en.wikipedia.org/wiki/Spline_(mathematics)" class="text-blue-500 underline">
+            https://en.wikipedia.org/wiki/Spline_(mathematics)
+          </a>. 
+          Acesso em: 10 out. 2023.
+        </li>
+        <li class="mb-2">
+          SUMMBIT. Bézier Curve Guide. Disponível em:{' '}
+          <a href="https://www.summbit.com/blog/bezier-curve-guide/" class="text-blue-500 underline">
+            https://www.summbit.com/blog/bezier-curve-guide/
+          </a>. 
+          Acesso em: 10 out. 2023.
+        </li>
+        <li class="mb-2">
+          YOUTUBE. Bézier Curves Explained. Disponível em:{' '} 
+          <a href="https://www.youtube.com/watch?v=enNfb6p3j_g" class="text-blue-500 underline">
+            https://www.youtube.com/watch?v=enNfb6p3j_g
+          </a>. 
+          Acesso em: 10 out. 2023.
+        </li>
+        <li class="mb-2">
+          YOUTUBE. The Beauty of Bézier Curves. Disponível em:{' '}
+          <a href="https://www.youtube.com/watch?v=jvPPXbo87ds" class="text-blue-500 underline">
+            https://www.youtube.com/watch?v=jvPPXbo87ds
+          </a>. 
+          Acesso em: 10 out. 2023.
+        </li>
+        <li class="mb-2">
+          YOUTUBE. Bézier Curves: A Primer. Disponível em: {' '}
+          <a href="https://www.youtube.com/watch?v=aVwxzDHniEw" class="text-blue-500 underline">
+            https://www.youtube.com/watch?v=aVwxzDHniEw
+          </a>. 
+          Acesso em: 10 out. 2023.
+        </li>
+      </ul>
+    </div>
+  )
+};
+
+export default References;
