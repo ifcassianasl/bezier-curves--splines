bd0c7eb3756ffd8e55734b3178e927868966790d
Cassiana <cassianasilveira64@gmail.com>
2024-09-16 21:28:36 -0300

    Add dynamic import

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,17 @@
 import Latex from 'react-latex-next';
 import BezierChart from './bezierChart';
+import dynamic from 'next/dynamic'
+
+const DynamicBezierChart = dynamic(() => import('./bezierChart'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+})
 
 const Main = () => {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <Latex>$P = P_{0} + t * (P_{1} - P_{0})$</Latex>
-      <BezierChart/>
+      <DynamicBezierChart/>
     </main>
   )
 } 
